83e612970607ba50d92f99d9fce8cbaca67ce3da
Mazurel <mateusz.mazur@yahoo.com>
2020-03-19 17:37:11 +0100

    Added google tests module part 2.

---
+++ tests/MB/ModbusExceptionTests.cpp
@@ -0,0 +1,18 @@
+//
+// Created by mateusz on 31.01.2020.
+//
+
+#include "gtest/gtest.h"
+#include "modbusException.hpp"
+#include "modbusUtils.hpp"
+
+TEST(ModbusException , Exists)
+{
+    EXPECT_TRUE(MB::ModbusException::exist({ 0x0A, 0x81, 0x02 }));
+    EXPECT_TRUE(MB::ModbusException::exist({ 0x01, MB::utils::WriteMultipleDiscreteOutputCoils | 0b10000000 , 0x02 }));
+}
+
+TEST(ModbusException, Other)
+{
+    EXPECT_EQ(MB::ModbusException({0x0A, 0x82, 0x02}).functionCode(), MB::utils::ReadDiscreteInputContacts);
+}
