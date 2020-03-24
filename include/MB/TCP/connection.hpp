a9bafd071c2e6446043368151989894b59c2286f
Mazurel <mateusz.mazur@yahoo.com>
2020-03-24 11:05:54 +0100

    Cmake fixes

---
+++ include/MB/TCP/connection.hpp
@@ -0,0 +1,82 @@
+// Modbus for c++ <https://github.com/Mazurel/Modbus>
+// Copyright (c) 2020 Mateusz Mazur aka Mazurel
+// Licensed under: MIT License <http://opensource.org/licenses/MIT>
+
+#ifndef PROTOCOLCONVERTER_CONNECTION_HPP
+#define PROTOCOLCONVERTER_CONNECTION_HPP
+
+#include <memory>
+#include <type_traits>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <libnet.h>
+#include <poll.h>
+#include <cerrno>
+
+#include "modbusRequest.hpp"
+#include "modbusResponse.hpp"
+#include "modbusException.hpp"
+
+namespace MB::TCP
+{
+class Connection
+{
+public:
+    static const unsigned int DefaultTCPTimeout = 500;
+
+private:
+    int _sockfd = -1;
+    uint16_t _messageID = 0;
+    int _timeout = Connection::DefaultTCPTimeout;
+
+public:
+    explicit Connection() noexcept : _sockfd(-1), _messageID(0) {};
+    explicit Connection(int sockfd) noexcept;
+    Connection(const Connection & copy) = delete;
+    Connection(Connection && moved) noexcept;
+    Connection& operator=(Connection &&other) noexcept
+    {
+        if (this == &other)
+            return *this;
+
+        if (_sockfd != -1 && _sockfd != other._sockfd)
+            ::close(_sockfd);
+
+        _sockfd = other._sockfd;
+        _messageID = other._messageID;
+        other._sockfd = -1;
+
+        return *this;
+    }
+
+    [[nodiscard]] int getSockfd() const
+    {
+        return _sockfd;
+    }
+
+    static Connection with(std::string addr, int port);
+
+    ~Connection();
+
+    void sendRequest(const MB::ModbusRequest& req);
+    void sendResponse(const MB::ModbusResponse& res);
+    void sendException(const MB::ModbusException& ex);
+
+    MB::ModbusRequest awaitRequest();
+    MB::ModbusResponse awaitResponse();
+
+    [[nodiscard]] uint16_t getMessageId() const
+    {
+        return _messageID;
+    }
+
+    void setMessageId(uint16_t messageId)
+    {
+        _messageID = messageId;
+    }
+};
+}
+
+
+#endif //PROTOCOLCONVERTER_CONNECTION_HPP
---
+++ include/MB/modbusException.hpp
@@ -0,0 +1,113 @@
+// Modbus for c++ <https://github.com/Mazurel/Modbus>
+// Copyright (c) 2020 Mateusz Mazur aka Mazurel
+// Licensed under: MIT License <http://opensource.org/licenses/MIT>
+
+#ifndef PROTOCOLCONVERTER_MODBUSEXCEPTION_HPP
+#define PROTOCOLCONVERTER_MODBUSEXCEPTION_HPP
+
+#include <cstdint>
+#include <vector>
+#include <stdexcept>
+#include <cstring>
+#include <optional>
+#include <memory>
+
+#include "modbusUtils.hpp"
+
+namespace MB
+{
+class ModbusException : public std::exception {
+private:
+    uint8_t _slaveId;
+    bool _validSlave;
+    utils::MBErrorCode _errorCode;
+    utils::MBFunctionCode _functionCode;
+
+public:
+    /**
+     * @brief Constructs Exception from raw data
+     * @param inputData is vector of bytes that will be be interpreted
+     * @param CRC based on this param method performs CRC calculation and throws exception if it is invalid
+     * @note if CRC = true input data needs to contain 2 CRC bytes on back (used in RS)
+     * */
+    explicit ModbusException(const std::vector<uint8_t>& inputData, bool CRC = false) noexcept;
+
+
+    // Constructs Exception based on error code, function code and slaveId
+    explicit ModbusException(utils::MBErrorCode errorCode, uint8_t slaveId = 0xFF, utils::MBFunctionCode functionCode = utils::Undefined)
+                                                            noexcept :
+                                                            _slaveId(slaveId),
+                                                            _validSlave(true),
+                                                            _errorCode(errorCode),
+                                                            _functionCode(functionCode)
+                                                            {}
+
+    /*
+     * Check if there is Modbus error in raw modbus input
+     * NOTE: this method doesn't detect invalid byte order, byte order is checked at ModbusRequest/ModbusResponse
+     * */
+    static bool exist(const std::vector<uint8_t>& inputData) noexcept
+    {
+        if (inputData.size() < 2) //TODO Figure out better solution to such mistake
+            return false;
+
+        return inputData[1] & 0b10000000;
+    }
+
+    /*
+     *  Returns attached SlaveID
+     *  NOTE: it is worth to check if slaveId is specified with isSlaveValid()
+     * */
+    [[nodiscard]] uint8_t slaveID() const noexcept {
+        return _slaveId;
+    }
+
+
+    // Checks if SlaveID is specified
+    [[nodiscard]] bool isSlaveValid() const noexcept {
+        return _validSlave;
+    }
+
+    // Sets SlaveID
+    void setSlaveID(uint8_t slaveId) noexcept
+    {
+        _validSlave = true;
+        _slaveId = slaveId;
+    }
+
+    // Returns detected error code
+    [[nodiscard]] utils::MBErrorCode getErrorCode() const noexcept {
+        return _errorCode;
+    }
+
+    /*
+     * This function is less optimal, it is just to be compatible with std::excepetion
+     * You should preferably use toString()
+     */
+    [[nodiscard]] const char * what () const noexcept override {
+        auto og = toString();
+        char* str = new char[og.size()];
+        stpcpy(str , og.c_str());
+        return str;
+    }
+
+    //Returns string representation of object
+    [[nodiscard]] std::string toString() const noexcept;
+    // Converts object to modbus byte representation
+    [[nodiscard]] std::vector<uint8_t> toRaw() const noexcept;
+
+    [[nodiscard]] utils::MBFunctionCode functionCode() const noexcept
+    {
+        return _functionCode;
+    }
+
+    void setFunctionCode(utils::MBFunctionCode functionCode) noexcept
+    {
+        _functionCode = functionCode;
+    }
+
+};
+}
+
+
+#endif //PROTOCOLCONVERTER_MODBUSEXCEPTION_HPP
