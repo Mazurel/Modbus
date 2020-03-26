027f76f1197496aeb1f8454360b2231dfccf10eb
Mazurel <mateusz.mazur@yahoo.com>
2020-03-26 09:48:20 +0100

    Tests fixes and small cmake changes

---
+++ tests/MB/ModbusCellTests.cpp
@@ -0,0 +1,21 @@
+// Modbus for c++ <https://github.com/Mazurel/Modbus>
+// Copyright (c) 2020 Mateusz Mazur aka Mazurel
+// Licensed under: MIT License <http://opensource.org/licenses/MIT>
+
+#include "gtest/gtest.h"
+#include "MB/modbusException.hpp"
+#include "MB/modbusUtils.hpp"
+#include "MB/modbusCell.hpp"
+
+TEST(ModbusCell, Manipulation)
+{
+    auto cell1 = MB::ModbusCell::initReg(12);
+    EXPECT_EQ(cell1.reg(), 12);
+    EXPECT_TRUE(cell1.coil());
+    cell1.coil() = false;
+    EXPECT_FALSE(cell1.coil());
+    EXPECT_EQ(cell1.reg(), 0);
+    cell1.reg() = 3;
+    EXPECT_EQ(cell1.reg(), 3);
+}
+
