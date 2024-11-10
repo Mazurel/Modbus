ca4eb0f97cbf6fab4a35f153dfa066930ae24e26
Mateusz Mazur <mateusz.mazur@e.email>
2024-11-10 13:42:06 +0100

    Clang format in the CI
    Signed-off-by: Mateusz Mazur <mateusz.mazur@e.email>

--- reformat-all.sh
+++ reformat-all.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-find . -name '*.cpp' -o -name '*.hpp' | xargs clang-format -i -style=file
+ALL_FORMATABLE_FILES=$(find src include tests example -name '*.cpp' -o -name '*.hpp')
+CLANG_FORMAT_ARGS="-style=file"
+
+echo "*** Modbus Clang Format Wrapper **"
+clang-format --version
+
+if [[ "$DRY" = "TRUE" ]]; then
+    diff_count=`clang-format --dry-run $CLANG_FORMAT_ARGS $ALL_FORMATABLE_FILES 2>&1 | wc -l`
+    exit $(($diff_count > 0))
+else
+    clang-format -i --verbose $CLANG_FORMAT_ARGS $ALL_FORMATABLE_FILES
+fi
